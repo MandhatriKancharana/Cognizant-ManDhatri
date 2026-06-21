cfacce97eac3a417edd92798cc8a1ecbda45d43d
Mandhatri Kancharana <kancharanadhatri@gmail.com>
2026-06-21 15:25:05 +0530

    Added Week-1 Module-1 and Module-2 assignments

---
+++ DeepSkilling/Week-1/Module-1 - Design Patterns and Principles/FactoryMethodPatternExample/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+public interface IDocument
+{
+    void Open();
+}
+
+public class WordDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Word Document Opened");
+    }
+}
+
+public class PdfDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("PDF Document Opened");
+    }
+}
+
+public class ExcelDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Excel Document Opened");
+    }
+}
+
+public abstract class DocumentFactory
+{
+    public abstract IDocument CreateDocument();
+}
+
+
+public class WordFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new WordDocument();
+    }
+}
+
+public class PdfFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
+
+public class ExcelFactory : DocumentFactory
+    {
+    public override IDocument CreateDocument()
+    {
+        return new ExcelDocument();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DocumentFactory wordFactory = new WordFactory();
+        IDocument wordDoc = wordFactory.CreateDocument();
+        wordDoc.Open();
+
+        DocumentFactory pdfFactory = new PdfFactory();
+        IDocument pdfDoc = pdfFactory.CreateDocument();
+        pdfDoc.Open();
+
+        DocumentFactory excelFactory = new ExcelFactory();
+        IDocument excelDoc = excelFactory.CreateDocument();
+        excelDoc.Open();
+    }
+}
---
+++ DeepSkilling/Week-1/Module-2 - Data Structures and Algorithms/EcommerceSearchExample/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+class Product
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string Category { get; set; }
+
+    public Product(int id, string name, string category)
+    {
+        ProductId = id;
+        ProductName = name;
+        Category = category;
+    }
+}
+
+class Program
+{
+    
+    static Product LinearSearch(Product[] products, int targetId)
+    {
+        foreach (Product product in products)
+        {
+            if (product.ProductId == targetId)
+                return product;
+        }
+
+        return null;
+    }
+
+    
+    static Product BinarySearch(Product[] products, int targetId)
+    {
+        int left = 0;
+        int right = products.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (products[mid].ProductId == targetId)
+                return products[mid];
+
+            if (products[mid].ProductId < targetId)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return null;
+    }
+
+    static void Main()
+    {
+        Product[] products =
+        {
+            new Product(101,"Laptop","Electronics"),
+            new Product(102,"Phone","Electronics"),
+            new Product(103,"Shoes","Fashion"),
+            new Product(104,"Watch","Accessories"),
+            new Product(105,"Bag","Fashion")
+        };
+
+        int searchId = 104;
+
+        Console.WriteLine("Linear Search:");
+
+        Product linearResult = LinearSearch(products, searchId);
+
+        if (linearResult != null)
+            Console.WriteLine($"Found: {linearResult.ProductName}");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Binary Search:");
+
+        Product binaryResult = BinarySearch(products, searchId);
+
+        if (binaryResult != null)
+            Console.WriteLine($"Found: {binaryResult.ProductName}");
+    }
+}
